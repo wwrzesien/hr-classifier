2739cbf9162bea90ddc2dd10134770cc69defbc2
Wojtek <wrzesien.ww@gmail.com>
2021-06-02 11:58:49 +0200

    add preprocess function

--- src/model_evaluate.R
+++ src/model_evaluate.R
@@ -1,3 +1,3 @@
-model_evaluate <- function(test_set, target_col = "target", predictions_col = "predicted_class") {
-   evalute(data = test_set, target_col = "target", predictions_col = "predicted_class")
+model_evaluate <- function(test_set, target_col = "target", prediction_cols = "predicted_class") {
+   evaluate(data = test_set, target_col = target_col, prediction_cols = prediction_cols, type = "binomial")
 }
